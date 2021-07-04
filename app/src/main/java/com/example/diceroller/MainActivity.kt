c43fea29fd4ca7edcdab14fb7e8fcf92b044054b
wesleyarizio <wesleyarizio26@gmail.com>
2021-07-04 02:57:51 -0300

    added first android project - dice roller - udacity course

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById<Button>(R.id.roll_button);
+
+        rollButton.setOnClickListener { rollDice() };
+    }
+
+    private fun rollDice() {
+        var drawableResource = when ((0..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        var diceImage: ImageView = findViewById<ImageView>(R.id.dice_image);
+        diceImage.setImageResource(drawableResource);
+    }
+}
